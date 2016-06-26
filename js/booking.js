d2551f6bf57c0cb23289e4a424e34b7f5b608b2b
zhengss33 <493164072@qq.com>
2016-06-26 22:53:13 +0800

    添加遮罩层

---
+++ js/booking.js
@@ -0,0 +1,31 @@
+$(function(){
+    showPopup();
+
+    $("#j_popup").find(".popup-txt").focus(function(){
+        if (this.value == this.defaultValue) {
+            this.value = " ";
+        }
+    }).blur(function() {
+        if (this.value == " ") {
+            this.value = this.defaultValue;
+        }
+    });
+});
+
+/*立即预订遮罩*/
+function showPopup(){
+    var $mask = $("#j_mask"),
+        $popUp = $("#j_popup");
+    $(".booking-btn").click(function(event) {
+        $mask.show();
+        $popUp.show();
+    });
+    $mask.click(function(){
+        $mask.hide();
+        $popUp.hide();
+    });
+    $popUp.find('.close').click(function(){
+        $mask.hide();
+        $popUp.hide();
+    });
+}
